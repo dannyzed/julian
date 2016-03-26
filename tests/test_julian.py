5af9a3ed44196cef8b93c46692346e84bed5819a
Daniel Zawada <zawadadaniel@gmail.com>
2016-03-26 12:15:00 -0600

    Added some documentation to the tests

--- tests/test_julian.py
+++ tests/test_julian.py
@@ -5,11 +5,18 @@ import julian
 
 class JulianTests(unittest.TestCase):
     def test_simple_to_mjd(self):
+        """
+        Tests a simple conversion from datetime to MJD
+        """
         input_dt = datetime(year=2004, day=1, month=1)
 
         self.assertAlmostEqual(53005, julian.to_jd(input_dt, 'mjd'))
 
     def test_simple_from_mjd(self):
+        """
+        Tests a simple conversion from mjd to datetime
+
+        """
         input_mjd = 53005.1
 
         dt = julian.from_jd(input_mjd, 'mjd')
@@ -19,6 +26,9 @@ class JulianTests(unittest.TestCase):
         self.assertEqual(dt.day, 1)
 
     def test_mjd_full(self):
+        """
+        Ensures that to_jd and from_jd are inverses for a large number of mjds.
+        """
         start_mjd = 54372
         mjd_delta = 0.017
         end_mjd = 55372
@@ -33,6 +43,9 @@ class JulianTests(unittest.TestCase):
             mjd += mjd_delta
 
     def test_jd_full(self):
+        """
+        Ensures that to_jd and from_jd are inverses for a large number of jd's
+        """
         start_jd = 2457471.93681 - 500
         jd_delta = 0.017
         end_jd = 2457471.93681
