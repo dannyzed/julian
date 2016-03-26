2992f21713688f7cda58129fc7dec716e09dee1a
Daniel Zawada <zawadadaniel@gmail.com>
2016-03-26 11:56:24 -0600

    Bump version number

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
   name='julian',
   packages=['julian'],
-  version='0.11',
+  version='0.12',
   description='Simple library for converting between Julian calendar dates and datetime objects',
   author='Daniel Zawada',
   author_email='zawadadaniel@gmail.com',
   url='https://github.com/dannyzed/julian',
-  download_url='https://github.com/dannyzed/julian/tarball/0.11',
+  download_url='https://github.com/dannyzed/julian/tarball/0.12',
   keywords=['julian', 'calendar', 'datetime'],
   classifiers=[],
 )
