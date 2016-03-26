88c4d0345a237257a59d8f6aa6fdc82029b0d1a3
Daniel Zawada <zawadadaniel@gmail.com>
2016-03-26 12:04:32 -0600

    Bump release, added pandoc

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
 
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
 setup(
   name='julian',
   packages=['julian'],
-  version='0.12',
+  version='0.13',
   description='Simple library for converting between Julian calendar dates and datetime objects',
+  long_description=long_description,
   author='Daniel Zawada',
   author_email='zawadadaniel@gmail.com',
   url='https://github.com/dannyzed/julian',
-  download_url='https://github.com/dannyzed/julian/tarball/0.12',
+  download_url='https://github.com/dannyzed/julian/tarball/0.13',
   keywords=['julian', 'calendar', 'datetime'],
   classifiers=[],
 )
