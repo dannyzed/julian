7ca7e25afe2704f072b38f8e464bc4e7d4d7d0d7
Daniel Zawada <zawadadaniel@gmail.com>
2016-03-26 13:01:16 -0600

    Update to not use description if pandoc is not available

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ try:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst')
 except(IOError, ImportError):
-    long_description = open('README.md').read()
+    long_description = ''
 
 
 setup(
   name='julian',
   packages=['julian'],
-  version='0.13',
+  version='0.14',
   description='Simple library for converting between Julian calendar dates and datetime objects',
   long_description=long_description,
   author='Daniel Zawada',
   author_email='zawadadaniel@gmail.com',
   url='https://github.com/dannyzed/julian',
-  download_url='https://github.com/dannyzed/julian/tarball/0.13',
+  download_url='https://github.com/dannyzed/julian/tarball/0.14',
   keywords=['julian', 'calendar', 'datetime'],
   classifiers=[],
 )
